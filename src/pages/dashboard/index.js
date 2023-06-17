0d73d4d6c4546b8e21d099710e501ffa783b45ae
Robert Bennet <132396394+UnknownCoderBoy@users.noreply.github.com>
2023-06-17 17:52:23 +0000

    Update 5 files

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,27 +1,90 @@
 import Draggable, { DraggableCore } from "react-draggable";
-import { Card, Space, FloatButton } from "antd";
-import { QuestionCircleOutlined, SyncOutlined } from "@ant-design/icons";
-import React from "react";
+import { Card, Button, FloatButton, Input } from "antd";
+import {
+  QuestionCircleOutlined,
+  SyncOutlined,
+  MinusOutlined,
+  YoutubeOutlined,
+} from "@ant-design/icons";
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import { useMediaQuery } from "react-responsive";
+
 import "./style.css";
+const { Search } = Input;
 
 export default function Dashboard() {
+  const [url, setURL] = useState("");
+  const [ytvisible, ytsetVisible] = useState(false);
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  const handleyt = () => {
+    ytsetVisible((prev) => !prev);
+  };
+
   return (
     <>
-      <Draggable className="drag">
-        <Space direction="vertical" size={16}>
-          <Card size="small" title="Small size card" style={{ width: 300 }}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
-          </Card>
-        </Space>
-      </Draggable>
+      {ytvisible && (
+        <div>
+          <Draggable bounds="html">
+            <Card
+              size="small"
+              title="Youtube Player"
+              extra={
+                <Button
+                  type="text"
+                  icon={<MinusOutlined />}
+                  onClick={handleyt}
+                />
+              }
+              style={{ width: "50%" }}
+            >
+              <Search
+                placeholder="input youtube link"
+                onSearch={setURL}
+                enterButton
+              />
+              {isDesktopOrLaptop && (
+                <ReactPlayer
+                  style={{
+                    marginTop: "5%",
+                    margin: "auto",
+                  }}
+                  url={url}
+                  controls={true}
+                  width={"100%"}
+                />
+              )}
+              {isTabletOrMobile && (
+                <ReactPlayer
+                  style={{
+                    marginTop: "5%",
+                    margin: "auto",
+                  }}
+                  url={url}
+                  controls={true}
+                  width={"100%"}
+                  height={"250px"}
+                />
+              )}
+            </Card>
+          </Draggable>
+        </div>
+      )}
 
-      <FloatButton.Group shape="square" style={{ bottom: "30%", right: 24 }}>
+      <FloatButton.Group shape="square">
         <FloatButton icon={<QuestionCircleOutlined />} />
         <FloatButton tooltip={<div>custom badge color</div>} />
         <FloatButton icon={<SyncOutlined />} />
-        <FloatButton />
+        <FloatButton
+          icon={<YoutubeOutlined />}
+          tooltip={<div>Toggle Youtube Player</div>}
+          onClick={handleyt}
+        />
       </FloatButton.Group>
     </>
   );
