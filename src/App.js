e946a9aef82397a8b2a57d5015b48d3060507043
Unknown Coder <132396394+UnknownCoderBoy@users.noreply.github.com>
2023-06-17 15:07:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "lib/routes";
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
---
+++ src/lib/routes.js
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "pages/dashboard";
+
+export const ROOT = "/";
+
+export const router = createBrowserRouter([
+  { path: ROOT, element: <Dashboard /> }
+]);
