676f9850f72c5061b626ce0d3d9342df3792324d
Robert Bennet <132396394+UnknownCoderBoy@users.noreply.github.com>
2023-06-17 11:16:22 +0000

    Update 7 files

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,33 @@
-import { Button } from "antd";
+import Draggable, { DraggableCore } from "react-draggable";
+import { Card, Space, FloatButton } from "antd";
+import { QuestionCircleOutlined, SyncOutlined } from "@ant-design/icons";
+import React from "react";
 
 export default function Dashboard() {
-  return <Button type="primary">Button</Button>;
+  return (
+    <>
+      <Draggable>
+        <Space direction="vertical" size={16}>
+          <Card size="small" title="Small size card" style={{ width: 300 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </Space>
+      </Draggable>
+
+      {/* <FloatButton.Group shape="circle" style={{ right: 24 }}>
+        <FloatButton icon={<QuestionCircleOutlined />} />
+        <FloatButton />
+        <FloatButton.BackTop visibilityHeight={0} />
+      </FloatButton.Group> */}
+
+      <FloatButton.Group shape="square" style={{ bottom: 500, right: 24 }}>
+        <FloatButton icon={<QuestionCircleOutlined />} />
+        <FloatButton tooltip={<div>custom badge color</div>} />
+        <FloatButton icon={<SyncOutlined />} />
+        <FloatButton />
+      </FloatButton.Group>
+    </>
+  );
 }
