d3e807f65a6a6fc115215a2f882be16d2c6f9c0f
Robert Bennet <132396394+UnknownCoderBoy@users.noreply.github.com>
2023-06-19 15:39:31 +0000

    Update 8 files

---
+++ src/widgets/jokes/Jokes.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { SmileOutlined } from "@ant-design/icons";
+import { Button, notification } from "antd";
+
+export default function Jokes() {
+  const [jokedata, fetchjoke] = useState(
+    "जिंदगी में थोड़े कांड भी करने चाहिये नहीं तो बुढ़ापे में गंगा में क्या धोओगे कच्छे"
+  );
+  const [api, contextHolder] = notification.useNotification();
+
+  const openNotification = () => {
+    fetchData();
+    api.open({
+      message: "Joke",
+      description: jokedata,
+      icon: <SmileOutlined style={{ color: "#108ee9" }} />,
+    });
+  };
+  const fetchData = async () => {
+    const url =
+      "https://hindi-jokes-api.onrender.com/jokes/?api_key=5a7b32b44725b9a69f271b79c763";
+    const response = await fetch(url);
+    const data = await response.json();
+    "<Emoji symbol=""/>"
+    fetchjoke(data.jokeContent);
+  };
+  return (
+    <>
+      {contextHolder}
+      <Button type="primary" onClick={openNotification} shape="circle"></Button>
+    </>
+  );
+}
--- src/widgets/pdf-viewer/PDFViewer.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-import FilePDF from "data/ieee-format.pdf";
-
-export default function PDFViewer() {
-  return <div>Hi</div>;
-}
