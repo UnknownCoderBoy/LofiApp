25a1d8cb7b80e5eb075dea97c554826a6e993f05
Robert Bennet <132396394+UnknownCoderBoy@users.noreply.github.com>
2023-06-19 13:28:55 +0000

    Update 6 files

---
+++ src/widgets/pdf-viewer/PDFViewer.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import FilePDF from "data/ieee-format.pdf";
+import PDFViewer from "pdf-viewer-reactjs";
+
+export default function PDFView() {
+  return (
+    // <PDFViewer
+    //   document={{
+    //     url: FilePDF,
+    //   }}
+    // />
+  );
+}
