a5043838ae5add38da38274186e15c53fe404360
Robert Bennet <132396394+UnknownCoderBoy@users.noreply.github.com>
2023-06-22 20:15:03 +0000

    Update style.css and Weather.js

--- src/widgets/weather/Weather.js
+++ src/widgets/weather/Weather.js
@@ -7,21 +7,24 @@ export default function Weather() {
 
   const fetchData = async () => {
     const API_KEY = "1b061b79c5539f8cafcbfdf517f257a2";
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=18.85&lon=73.88&units=metric&appid=${API_KEY}`;
+    const Location = "Rajgurunagar";
+    const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${Location}&appid=${API_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-    const { name, main, weather } = data;
-    const { temp, temp_min, temp_max } = main;
-    const { icon, mains, description } = weather[0];
+    const { name, weather } = data;
+    const { temp, temp_min, temp_max } = data.main;
+    const { id, icon, main, description } = weather[0];
+
     fetchweather({
       ...weatherdata,
       name: name,
       temp: temp,
       min_temp: temp_min,
       max_temp: temp_max,
+      id: id,
       icon: icon,
-      main: mains,
+      main: main,
       description: description,
     });
     console.log(weatherdata.temp);
@@ -31,21 +34,86 @@ export default function Weather() {
     fetchData();
   }, []);
 
+  const nowdate = new Date();
+  var currentMonth = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    currentDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    month = nowdate.getMonth(),
+    date = nowdate.getDate(),
+    hour = nowdate.getHours(),
+    minutes = nowdate.getMinutes(),
+    day = nowdate.getDay(),
+    amPM = "";
+
+  if (hour === 0) {
+    hour = 12;
+  }
+  if (hour > 12) {
+    hour -= 12;
+    amPM = "PM";
+  } else {
+    amPM = "AM";
+  }
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  const classname = () => {
+    var iconClass = "wi-owm-";
+
+    if (amPM === "PM") {
+      iconClass += "night-";
+    } else {
+      iconClass += "day-";
+    }
+    return iconClass;
+  };
+
   return (
     <>
-      <div className="temperature">
-        <Text strong className="celsius">
-          {weatherdata.temp} <span>&deg;</span> C
-        </Text>
-        <img
-          style={{
-            width: "50px",
-            height: "50px",
-          }}
-          // src={`http://openweathermap.org/img/w/10d.png`}
-          src={`http://openweathermap.org/img/wn/${weatherdata.icon}@2x.png`}
-          alt=""
-        />
+      <div id="weather-app" className="weather-app">
+        <i
+          className={`weather-icon wi wi-fw ${classname()}${weatherdata.id}`}
+        ></i>
+        <div className="date-time">
+          <div className="time">
+            {hour + ":" + minutes}
+            <span>{amPM}</span>
+          </div>
+          <div className="date">
+            <div className="day">{currentDay[day]}</div>
+            <div className="monDate">{currentMonth[month] + " " + date}</div>
+          </div>
+        </div>
+
+        <div className="location-weather">
+          <div className="weather-status">
+            <div className="city">{weatherdata.main}</div>
+            <div className="forcast">{weatherdata.description}</div>
+          </div>
+          <div className="weather-temp">
+            <div className="temperature">{Math.trunc(weatherdata.temp)}</div>
+            <div className="high-low">
+              <div className="high">{weatherdata.max_temp}</div>
+              <div className="low">{weatherdata.min_temp}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
