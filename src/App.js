da7793ab2e3a5fcf0c663e4483288233457551b0
Robert Bennet <132396394+UnknownCoderBoy@users.noreply.github.com>
2023-07-21 05:13:54 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ export default function App() {
       <RouterProvider router={router} />
       <AnimatedCursor
         innerSize={10}
-        outerSize={10}
+        outerSize={18}
         color="193, 11, 111"
         outerAlpha={0.4}
-        innerScale={0.7}
-        outerScale={5}
+        innerScale={0.8}
+        outerScale={2}
         style={{
           zIndex: "200",
         }}
